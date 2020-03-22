7d71bdca11f53264908f308713aa07be36350358
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-22 13:11:13 +0100

    Update Main.php

--- src/Crow-Balde/Main.php
+++ src/Crow-Balde/Main.php
@@ -1,3 +1,14 @@
 <?php
 
-namespace Crow-Balde;
+namespace ;
+
+use pocketmine\plugin\PluginBase;
+use Crow\SimpleGM\command\Broadcast;
+
+class Main extends PluginBase{
+
+	public function onEnable() {
+		$this->getLogger()->info("§a[Broadcast]§4 Plugin Ist Fertig Geladen");
+        $this->getServer()->getCommandMap()->register("Broadcast", new Broadcast($this));
+	}
+}
